ba0f69d98c67c187c83803657697e64a712960d0
albert7617 <albert860728@gmail.com>
2020-12-01 00:11:11 +0800

    Add delta plot, add hover legend.

--- project_2/raw/process.py
+++ project_2/raw/process.py
@@ -1,16 +1,31 @@
 import csv
 
-cw = open('nuclear.csv', 'w', newline='', encoding='utf8')
-csvWriter = csv.writer(cw)
-csvWriter.writerow(['Country', 'Year', 'TWh'])
+cw1 = open('nuclear.csv', 'w', newline='', encoding='utf8')
+csvWriter1 = csv.writer(cw1)
+csvWriter1.writerow(['Country', 'Year', 'TWh'])
+
+cw2 = open('nuclear_delta.csv', 'w', newline='', encoding='utf8')
+csvWriter2 = csv.writer(cw2)
+csvWriter2.writerow(['Country', 'Year', 'Delta'])
+
 with open('nuclear_raw.csv', newline='', encoding='utf8') as c:
   cr = csv.reader(c, delimiter=',')
   header = next(cr)
   for row in cr:
     it = iter(row)
     country = next(it)
+    previous = 0.0
     for head, col in zip(header[1:], it):
       if not (col == 'n/a' or col == '-'):
-        # print(country, head, 0.0 if col == 'n/a' or col == '-' else 0.05 if col == '^' else col) 
-        csvWriter.writerow([country, head, 0.05 if col == '^' else col])
-cw.close()
+        csvWriter1.writerow([country, head, 0.05 if col == '^' else col])
+        if col == '^':
+          floatCol = 0.05
+        else:
+          floatCol = float(col)
+        if previous == 0:
+          csvWriter2.writerow([country, int(head)+1, 'Infinity'])
+        else:
+          csvWriter2.writerow([country, int(head)+1, '%.3f' % (100*(floatCol-previous)/previous)])
+        previous = floatCol
+cw1.close()
+cw2.close()
